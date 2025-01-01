742e5d615c335ec52d3663a27487bfdeae058099
Ayoub El hioui <ael-hiou@c3r2p4.1337.ma>
2025-01-01 12:53:29 +0100

    working on the parser

--- Computor-v1.cpp
+++ Computor-v1.cpp
@@ -1,6 +1,9 @@
 #include "EquationParser/EquationParser.hpp" 
 
 int main(int ac, char* av[]) {
+    
+    if (ac != 2)
+        throw invalid_argument("Invalid number of arguments.");
     EquationParser equationParser;
     string leftSide, rightSide;
     vector<string> result = equationParser.splitByDelimiter(av[1], '=');
